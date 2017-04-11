1fdc0d74569877db4cb48cc4c148700a3fa9e8d3
russotx <russotx@gmail.com>
2017-04-10 22:04:15 -0500

    set up files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded.');
+
+exports.twitterKeys = {
+    consumer_key : '';
+    consumer_secret : '';
+    access_token_key : '';
+    access_token_secret : '';
+}
+
+
---
+++ liri.js
@@ -0,0 +1,61 @@
+/*
+
+    At the top of the liri.js file, write the code you need to grab the data 
+    from keys.js. Then store the keys in a variable.
+
+    Make it so liri.js can take in one of the following commands:
+
+    - my-tweets
+
+    - spotify-this-song
+
+    - movie-this
+
+    - do-what-it-says
+
+
+    
+    node liri.js my-tweets 
+    show last 20 tweets and when they were created
+
+    node liri.js spotify-this-song 'song name'
+    show song info:
+        artist(s)
+        the song's name
+        a preview link of the song from Spotify
+        the album that the song is from
+        or default to ('The Sign' by Ace of Base)
+
+    node liri.js movie-this 'movie name'
+    output:
+        title
+        year
+        imdb rating
+        country where produced
+        language
+        plot
+        actors
+        rotten tomatoes rating
+        rotten tomatoes url
+
+    node liri.js do-what-it-says
+        take text from random.txt and run the appropriate command
+
+    bonus: output to a text file.
+
+ */
+
+var twitter = require('keys.js');
+
+var allKeys = 
+
+
+
+
+
+
+
+
+
+
+
